eada4ecb48acdcfc3c89b2587a7098c3078bde3a
Timmi <123Wsxzxc>
2017-10-12 23:17:57 +0200

    First classes

---
+++ src/Ciphertext.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Timmi on 12-Oct-17.
+ */
+class Ciphertext {
+    private String ciphertext;
+
+    public String getCiphertext() {
+        return ciphertext;
+    }
+
+    public void setCiphertext(String ciphertext) {
+        this.ciphertext = ciphertext;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Timmi on 12-Oct-17.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
